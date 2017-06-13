f04fb411a08f1f2fdae50d5fcafdfd485e892aa3
Ryan VanGundy <vangundy@bounceexchange.com>
2017-06-12 23:04:41 -0400

    Demo 1

---
+++ README.md
@@ -0,0 +1,15 @@
+# Configuring Docker
+* Configuration is an important step in deploying Docker containers
+* Containers "run the same in all environments", but this isn't entirely true, often (usually) applications need special configuration per environment
+* If everything was built to be containerized, things would be easier
+
+## Ideal:
+* All configuration takes place as environment variables
+* Applications or other services run _exactly_ the same in all environments
+* The entire chain of container deployment secured
+
+## Demo 1: A Perfect World
+
+## Demo 2: Environment Variables
+
+## Demo 3: 
---
+++ demo1/index.js
@@ -0,0 +1,35 @@
+/******************
+ *  Dependencies  *
+ ******************/
+
+var http = require('http');
+
+/***************
+ *  Variables  *
+ ***************/
+
+var firstName = process.env.FIRST_NAME;
+var lastName = process.env.LAST_NAME;
+var gravatarUrl = process.env.GRAVATAR_URL;
+var port = process.env.PORT;
+
+/*************
+ *  Content  *
+ *************/
+
+var content = `<html><body>
+	<h1>This homepage was made just for you by</h1>
+	<h3><i>${firstName} ${lastName}</i></h3>
+	<img src="${gravatarUrl}">
+</body></html>`;
+
+/************
+ *  Server  *
+ ************/
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(content);
+}).listen(port);
+
+console.log('Server running on port ' + port);
